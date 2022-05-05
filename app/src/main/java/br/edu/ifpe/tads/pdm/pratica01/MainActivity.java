47a9fc8dc938caf26031ea09edcaef203b26f7ae
jonasfp <jfp@discente.ifpe.edu.br>
2022-05-05 09:27:29 -0300

    3º commit - Parte 4

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
@@ -2,13 +2,25 @@ package br.edu.ifpe.tads.pdm.pratica01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "Pratica01.Messagege";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
